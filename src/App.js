81d381583e3f1fb90f93e93789703b3f616182b6
Alexey Volkov <alexey.a.volkov@pm.me>
2021-03-02 16:59:36 +0300

    add structure
    initial commit, add base components, json database file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import jsonData from './websiteData.json'
+import Rows from './components/Rows/Rows'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <React.Fragment>
+      <header>
+        <h1>{jsonData.name}</h1>
       </header>
-    </div>
-  );
+      <main>
+        <Rows rows={jsonData.methods} />
+      </main>
+      <footer>
+        <ul>
+          <li>
+            <a
+              href="https://spb.hh.ru/resume/ff5930bbff033a8c5e0039ed1f47376b7a4d51"
+              rel="nofollow noopener"
+              lang="ru"
+              title="Резюме на hh.ru"
+            >
+              hh.ru
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/2plus2is4/"
+              rel="nofollow noopener"
+              title="Hire in LinkedIn"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/2plus2is4/"
+              rel="nofollow noopener"
+              title="Hire in LinkedIn"
+            >
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
