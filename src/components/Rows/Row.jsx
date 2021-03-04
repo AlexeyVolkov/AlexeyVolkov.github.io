121b61e5a50272f66fbfb4393b525b3c72516bcf
Alexey Volkov <alexey.a.volkov@pm.me>
2021-03-04 14:08:09 +0300

    add links code

--- src/components/Rows/Row.jsx
+++ src/components/Rows/Row.jsx
@@ -3,39 +3,35 @@ import Code from '../Code/Code'
 function Row(props) {
   return (
     <>
-      <dt className={`col ${props.row.name}`}>{props.row.title}</dt>
+      <dt className={`col ${props.row.name}`}>
+        <h2>{props.row.title}</h2>
+      </dt>
       <dd>
         <section className="row">
-          <Code
-            link={props.row['old-code-url']}
-            className="col-12 col-sm-6"
-            key={props.row['old-code-url']}
-          />
-          <Code
-            link={props.row['new-code-url']}
-            className="col-12 col-sm-6"
-            key={props.row['new-code-url']}
-          />
+          <Code link={props.row['old-code-url']} className="col-12 col-sm-6" />
+          <Code link={props.row['new-code-url']} className="col-12 col-sm-6" />
         </section>
         <section>
           <nav>
-            <h3>Links</h3>
-            {props.row.links.map((linkSection) => {
-              return (
-                <>
-                  {linkSection.title ? <h4>{linkSection.title}</h4> : ''}
-                  <ol>
-                    {linkSection.links.map((link) => (
-                      <li key={link.href}>
-                        <a href={link.href} title={link?.title}>
-                          {link.text}
-                        </a>
-                      </li>
-                    ))}
-                  </ol>
-                </>
-              )
-            })}
+            <h3>Resources</h3>
+            <div className="row">
+              {props.row.links.map((linkSection) => {
+                return (
+                  <div className="col">
+                    {linkSection.title ? <h4>{linkSection.title}</h4> : ''}
+                    <ol>
+                      {linkSection.links.map((link) => (
+                        <li key={link.href}>
+                          <a href={link.href} title={link?.title}>
+                            {link.text}
+                          </a>
+                        </li>
+                      ))}
+                    </ol>
+                  </div>
+                )
+              })}
+            </div>
           </nav>
         </section>
       </dd>
