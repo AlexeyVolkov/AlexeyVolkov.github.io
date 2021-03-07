41750da179e76bfe48481a698e94bfb5dd1506a0
Alexey Volkov <alexey.a.volkov@pm.me>
2021-03-07 13:09:55 +0300

    links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ function Header(props) {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="/">
           <s>{props.name}</s>
         </a>
         <button
